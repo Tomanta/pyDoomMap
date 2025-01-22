5507ae271fdc45683d144ad944fa722ceb26828f
Tomanta <bzednick@gmail.com>
2025-01-21 20:23:26 -0600

    Added constants for reading the parts we need. Added Linedef dataclass.

--- src/wad/mapdata.py
+++ src/wad/mapdata.py
@@ -5,3 +5,14 @@ from dataclasses import dataclass
 class Vertex:
     x: int
     y: int
+
+
+@dataclass
+class Linedef:
+    start_vertex: int
+    end_vertex: int
+    flags: int
+    special: int
+    tag: int
+    front_sidedef: int
+    back_sidedef: int
--- src/wad/wadfile.py
+++ src/wad/wadfile.py
@@ -1,4 +1,13 @@
+from typing import Final
+import struct
 from dataclasses import dataclass
+
+
+# All layouts are read in little endian
+WAD_HEADER_LAYOUT: Final[str] = "<4s i i"  # 4 character string and two signed ints
+DIRECTORY_LAYOUT: Final[str] = "<i i 8s"  # 2 ints and an 8 character string
+VERTEX_LAYOUT: Final[str] = "<h h"  # two signed short ints
+LINEDEF_LAYOUT: Final[str] = "<h h h h h h h"  # 8 signed short ints
 
 
 @dataclass
@@ -6,3 +15,8 @@ class WadHeader:
     filetype: str
     numlumps: int
     directory_offset: int
+
+
+class WadReader:
+    def __init__(self, wadfile: str):
+        self.wadfile = wadfile
